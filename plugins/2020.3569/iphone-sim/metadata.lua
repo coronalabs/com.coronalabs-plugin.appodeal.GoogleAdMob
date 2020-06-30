aadfa38285e272d433782f878e13fc2d7b078506
Vlad Shcherban <vlad@coronalabs.com>
2020-06-30 10:56:28 +0300

    Updating binaries

---
+++ plugins/2020.3569/iphone-sim/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDGoogleAdMobAdapter', },
+		frameworks = { 'GoogleAppMeasurement', 'GoogleMobileAds', 'FBLPromises', 'GoogleUtilities', 'nanopb', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata

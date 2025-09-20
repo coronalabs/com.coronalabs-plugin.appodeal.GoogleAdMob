788d6edf2ad1219a1bd2f7bbbf98c17159d7b1a3
Scott Harrison <scottrules44@gmail.com>
2025-09-19 23:32:37 -0400

    Fixes to Network Framework

--- plugins/2018.3326/iphone/metadata.lua
+++ plugins/2018.3326/iphone/metadata.lua
@@ -5,7 +5,7 @@ local metadata =
 		format = 'staticLibrary',
 		staticLibs = { 'APDGoogleAdMobAdapter', },
 		frameworks = { 'GoogleAppMeasurement', 'GoogleAppMeasurementIdentitySupport', 'GoogleMobileAds', 'FBLPromises', 'GoogleUtilities', 'nanopb', "UserMessagingPlatform" },
-		frameworksOptional = {},
+		frameworksOptional = {"Network"},
 	},
 }
 
